50a9c06fda2c1fc29d19e421b95223e10a35991d
Tanim Sk <sktanim5800@gmail.com>
2024-08-11 13:44:25 +0600

    project init

---
+++ travonus_cache_server/travonus_cache_server/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for travonus_cache_server project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travonus_cache_server.settings')
+
+application = get_asgi_application()
