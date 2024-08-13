464713df67efb366cec210399a0023b4c2e0e6f7
Tanim Sk <sktanim5800@gmail.com>
2024-08-13 13:25:27 +0600

    celery added

--- travonus_cache_server/api_handler/urls.py
+++ travonus_cache_server/api_handler/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from api_handler.views import AirSearch, CacheAirSearch, FareRules, PricingDetails, PricingCalendar
+from api_handler.views import CacheAirSearch, FareRules, PricingDetails, PricingCalendar
 from api_handler.flyhub.api import authenticate as flyhub_auth
 from api_handler.sabre.api import authenticate as sabre_auth
 
 urlpatterns = [
-    path("air_search/", AirSearch.as_view(), name="air_search"),
     path("cache_air_search/", CacheAirSearch.as_view(), name="air_search"),
     path("pricing_calendar", PricingCalendar.as_view(), name="pricing_calendar"),
     path("air_rules/", FareRules.as_view(), name="air_rules"),
