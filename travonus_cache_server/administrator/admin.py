1363990573c3d6e5a94cb4b94e0118bc66522666
Tanim Sk <sktanim5800@gmail.com>
2024-11-28 11:17:35 +0600

    admin

--- travonus_cache_server/administrator/admin.py
+++ travonus_cache_server/administrator/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from administrator.models import MobileAdminInfo
 
-# Register your models here.
+@admin.register(MobileAdminInfo)
+class MobileAdminInfoAdmin(admin.ModelAdmin):
+    list_display = ["admin_markup", "updated_on"]    
+    
