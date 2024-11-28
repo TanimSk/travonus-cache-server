a048e3a39cd29abaa218649de29f7f938acebcea
Tanim Sk <sktanim5800@gmail.com>
2024-11-28 11:41:03 +0600

    removed sabre session

--- travonus_cache_server/api_handler/sabre/api.py
+++ travonus_cache_server/api_handler/sabre/api.py
@@ -9,16 +9,15 @@ from decimal import Decimal
 from api_handler.sabre.translators import (
     air_search_translate,
     search_result_translate,
-    air_rules_inject_translate,
     air_rules_mini_inject_translate,
     air_rules_mini_result_translate,
-    air_rules_result_translate,
     air_pricing_details_inject_translate,
     air_pricing_details_result_translate,
     flight_booking_inject_translate,
     flight_booking_result_translate,
     flight_pre_booking_result_translate,
 )
+
 # from agent.models import AgentMarkup
 from api_handler.sabre.serializers import AuthenticationSerializer
 from api_handler.sabre import urls
@@ -72,7 +71,7 @@ def air_search(
     search_params: dict,
     tracing_id: str,
     admin_markup: Decimal,
-    agent_markup_instance = None,
+    agent_markup_instance=None,
 ):
     # general format to sabre native format
     body = air_search_translate(search_params=search_params)
@@ -88,10 +87,6 @@ def air_search(
         headers={"Authorization": f"Bearer {token}"},
         ssl=False,
     )
-
-    # create a thread for calling `create_session` function
-    create_session_thread = threading.Thread(target=create_session)
-    create_session_thread.start()
 
     return search_result_translate(
         results=api_response,
@@ -138,30 +133,10 @@ def mini_air_rules(rules_params: dict):
     )
 
 
-def air_rules(rules_params: dict):
-    body_list = air_rules_inject_translate(rules_params=rules_params)
-    result = []
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        futures = [
-            executor.submit(
-                air_rules_individual,
-                body,
-            )
-            for body in body_list
-        ]
-
-        for future in concurrent.futures.as_completed(futures):
-            result.append(future.result())
-
-    print(result)
-    return air_rules_result_translate(result)
-
-
 def pricing_details(
     pricing_params: dict,
     admin_markup: Decimal,
-    agent_markup_instance = None,
+    agent_markup_instance=None,
 ):
     body = air_pricing_details_inject_translate(pricing_params=pricing_params)
 
