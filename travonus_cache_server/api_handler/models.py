432b20b69fe26a7a0dd0cb96ff297103107c8bd7
tanimsk <sktanim5800@gmail.com>
2025-04-05 11:28:35 +0600

    celery task

--- travonus_cache_server/api_handler/models.py
+++ travonus_cache_server/api_handler/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class ApiCredentials(models.Model):
     api_name = models.CharField(max_length=100, unique=True)
     token = models.TextField()
-    created_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
     expiry_date = models.DateTimeField()
 
 
